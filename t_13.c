25f2099207085cfa6de6dd48be201088b5603d58
zhouyifan <552094467@qq.com>
2016-06-30 20:58:30 +0800

    modify something

---
+++ t_13.c
@@ -0,0 +1,18 @@
+##include <stdio.h>
+// 给定一个整数num，( 1<=num<=3999)，将整数转换成罗马数字。
+int main() {
+  /*
+  int I = 1;
+  int V = 5;
+  int X = 10;
+  int L = 50;
+  int C = 100;
+  int D = 500;
+  int M = 1000;
+  */
+
+
+  int number;
+  scanf("%d", &number);
+  return 0;
+}
